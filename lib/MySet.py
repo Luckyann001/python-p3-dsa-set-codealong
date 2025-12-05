8ec08bc67f970789559fd43a8d0968ef3685f154
Luckyann Kagendo <kagendoluckyann@gmail.com>
2025-12-05 22:19:39 +0300

    updated files

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,21 @@
 class MySet:
+    def __init__(self, initial=None):
+        # Backing store required by the tests
+        self.dictionary = {}
 
-    pass
+        if initial:
+            for value in initial:
+                self.dictionary[value] = True  # placeholder
+
+    def add(self, value):
+        self.dictionary[value] = True
+
+    def delete(self, value):
+        if value in self.dictionary:
+            del self.dictionary[value]
+
+    def has(self, value):
+        return value in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
